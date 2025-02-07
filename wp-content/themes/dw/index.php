9c55c8ad294bd8ce51a87655722c664a878e840b
س <s@ss-MacBook-Air.local>
2025-02-07 16:34:57 +0100

    Message décrivant les changements

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Salut</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= wp_title( '·', false, 'right' ) . get_bloginfo( 'name' ) ?>></title>
 </head>
 <body>
-<h1><?= get_bloginfo('name') ?></h1>
+<h1><?= get_bloginfo( 'name' ) ?></h1>
+<p><?= get_bloginfo( 'description' ) ?></p>
 </body>
 </html>
